3fba4d719956412a0a8feb6e2ae4fd8eeb4901f2
bryanseros <bryanseros@gmail.com>
2022-08-10 20:54:39 -0500

    No permitir crear usuarios existentes

--- FileServerBack/src/main/java/com/app/back/domain/usecase/user/RegisterUserUseCase.java
+++ FileServerBack/src/main/java/com/app/back/domain/usecase/user/RegisterUserUseCase.java
@@ -3,6 +3,8 @@ package com.app.back.domain.usecase.user;
 import com.app.back.domain.model.user.User;
 import com.app.back.domain.model.user.gateways.PasswordEncryptService;
 import com.app.back.domain.model.user.gateways.UserService;
+import com.app.back.infraestructure.drivenadapter.mongo.adapter.UserMapper;
+import com.app.back.infraestructure.drivenadapter.mongo.entity.UserEntity;
 import lombok.AllArgsConstructor;
 import reactor.core.publisher.Mono;
 
@@ -13,7 +15,12 @@ public class RegisterUserUseCase {
     private PasswordEncryptService passwordEncryptService;
     private UserService userRepository;
 
-    public Mono<User> register(User user){
+    public Mono<User> register(User user) {
+        UserEntity userEnt = UserMapper.toData(user);
+        Mono<UserEntity> userExist = userRepository.findByEmail(userEnt.getEmail());
+        if(userExist != null){
+            throw new RuntimeException("User Exist");
+        }
         return Mono.fromCallable(()->user)
                 .onErrorResume(e -> Mono.error(e))
                 .map(userIniciate -> {
